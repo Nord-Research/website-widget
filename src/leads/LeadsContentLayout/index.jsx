d6576a2e6255c843333981356be4fc8a42f8c843
Maicon Silva <maiconrs95@gmail.com>
2021-02-13 17:12:20 -0300

    feat: add leads content layout components

---
+++ src/leads/LeadsContentLayout/index.jsx
@@ -0,0 +1,11 @@
+import { h } from "preact";
+import "./styles.css";
+
+const LeadsContentLayout = ({ children }) => (
+  <div className="container">
+    {children}
+  </div>
+);
+
+
+export default LeadsContentLayout;
