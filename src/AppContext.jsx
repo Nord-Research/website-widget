68467474fb6496c41aa22e54f325fcf9759f8d7d
Maicon Silva <maiconrs95@gmail.com>
2021-02-13 17:11:10 -0300

    add default use app context hook

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,9 +1,12 @@
 import { h, createContext } from "preact";
+import { useContext } from "preact/hooks";
+
 
 export const ConfigContext = createContext({});
 
-export const AppContext = ({ children, config }) => {
-  return (
-    <ConfigContext.Provider value={config}>{children}</ConfigContext.Provider>
-  );
-};
+export const AppContext = ({ children, config }) => (
+  <ConfigContext.Provider value={config}>{children}</ConfigContext.Provider>
+);
+
+
+export const useAppContextConsumer = () => useContext(ConfigContext);
