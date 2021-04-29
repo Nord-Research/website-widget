6cefa8265ac42ed78d8d66abfaa5b21d83aeded5
bissuh <bissuh@gmail.com>
2021-04-29 00:04:43 -0300

    feat(slideshow): remove lateral fades.

--- src/slideshow/styles.js
+++ src/slideshow/styles.js
@@ -1,5 +1,4 @@
-import styled, { keyframes, css } from 'styled-components';
-
+import styled, { keyframes, css } from "styled-components";
 
 const scrollAnimation = ({ width, imagesCount }) => keyframes`
   0% {
@@ -17,26 +16,6 @@ export const Slider = styled.div`
   overflow: hidden;
   position: relative;
   width: 100%;
-
-  &::after, &::before {
-    background: linear-gradient(to right, white 0%, rgba(255, 255, 255, 0) 100%);
-    content: "";
-    height: 100%;
-    position: absolute;
-    width: 200px;
-    z-index: 2;
-  }
-
-  &::after {
-    right: 0;
-    top: 0;
-    transform: rotateZ(180deg);
-  }
-
-  &::before {
-    left: 0;
-    top: 0;
-  }
 `;
 
 export const SlideTrack = styled.div`
@@ -46,4 +25,4 @@ export const SlideTrack = styled.div`
     animation-duration: ${duration};
     width: calc(${width}px * ${imagesCount * 2});
   `}
-`
+`;
