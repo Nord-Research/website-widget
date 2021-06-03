4758bd6da06571f8fe3cf6e86df009d6106d13cb
Maicon Silva <maiconrs95@gmail.com>
2021-06-03 15:42:30 -0300

    feat(recommendations): update mobile styles

--- src/recommendations/components/Table/index.jsx
+++ src/recommendations/components/Table/index.jsx
@@ -1,32 +1,85 @@
 import { h } from "preact";
 
-import { TABLE_HEAD } from '../../constants/table';
+import { HEAD_AREAS,HEAD_LABELS } from '../../constants/table';
 
 import './styles.css';
 import './thead.css';
+import './tbody.css';
+import './trow.css';
+import './card.css';
+
+const MOCKED_ROWS = [
+  {
+    order: 1,
+    logo: 'Logo',
+    ticker: 'Ticker',
+    variation: '1,5%',
+    currentPrice: 'R$ 25,35',
+    maxPrice: 'R$ 30,00',
+    recommendation: 'Comprar',
+    alocation: '9,4%'
+  },
+  {
+    order: 2,
+    logo: 'Logo',
+    ticker: 'Ticker',
+    variation: '1,5%',
+    currentPrice: 'R$ 25,35',
+    maxPrice: 'R$ 30,00',
+    recommendation: 'Comprar',
+    alocation: '9,4%'
+  },
+  {
+    order: 3,
+    logo: 'Logo',
+    ticker: 'Ticker',
+    variation: '1,5%',
+    currentPrice: 'R$ 25,35',
+    maxPrice: 'R$ 30,00',
+    recommendation: 'Comprar',
+    alocation: '9,4%'
+  },
+]
 
 export const Table = ({ }) => (
   <div className="container">
     <div className="thead">
       <div className="tcolumns">
-        {TABLE_HEAD.map(label => (
+        {Object.values(HEAD_LABELS).map((label) => (
           <span>{label}</span>
         ))}
       </div>
     </div>
 
     <div className="tbody">
-      {Array.from({ length: 5 }).fill('').map(() => (
+      {MOCKED_ROWS.map((item) => (
         <div className="trow">
-          <span>1#</span>
-          <div className="tcolumns">
-            <span>Logo</span>
-            <span>Ticker</span>
-            <span>1.5%</span>
-            <span>R$ 25,35</span>
-            <span>R$ 30,00</span>
-            <span>Comprar</span>
-            <span>9,4%</span>
+          <span className="order">#{item.order}</span>
+          <div className="tcolumns card">
+            <span className="logo">{item.logo}</span>
+            <span className="ticker row-item">
+              <p className="card-title">{HEAD_LABELS[HEAD_AREAS.TICKER]}</p>
+              <p className="value">{item.ticker}</p>
+            </span>
+            <span className="variation row-item">
+              <p className="card-title">{HEAD_LABELS[HEAD_AREAS.VARIATION]}</p>
+              <p>{item.variation}</p>
+            </span>
+            <span className="current-price row-item">
+              <p className="card-title">{HEAD_LABELS[HEAD_AREAS.CURRENT_PRICE]}</p>
+              <p>{item.currentPrice}</p>
+            </span>
+            <span className="max-price row-item">
+              <p className="card-title">{HEAD_LABELS[HEAD_AREAS.MAX_PRICE]}</p>
+              <p>{item.maxPrice}</p>
+            </span>
+            <span className="recommendation">
+              <p>{item.recommendation}</p>
+            </span>
+            <span className="alocation row-item">
+              <p className="card-title">{HEAD_LABELS[HEAD_AREAS.ALOCATION]}</p>
+              <p>{item.alocation}</p>
+            </span>
           </div>
         </div>
       ))}
--- src/recommendations/constants/table.js
+++ src/recommendations/constants/table.js
@@ -1,9 +1,19 @@
-export const TABLE_HEAD = [
-  'Empresa',
-  'Ticker',
-  'Variação do dia',
-  'Preço atual',
-  'Preço teto',
-  'Recomendação',
-  'Alocação',
-];
+export const HEAD_AREAS = {
+  LOGO: 'logo',
+  TICKER: 'ticker',
+  VARIATION: 'variation',
+  CURRENT_PRICE: 'current-price',
+  MAX_PRICE: 'max-price',
+  RECOMMENDATION: 'recommendation',
+  ALOCATION: 'alocation',
+}
+
+export const HEAD_LABELS = {
+  [HEAD_AREAS.LOGO]: 'Empresa',
+  [HEAD_AREAS.TICKER]: 'Ticker',
+  [HEAD_AREAS.VARIATION]: 'Variação do dia',
+  [HEAD_AREAS.CURRENT_PRICE]: 'Preço atual',
+  [HEAD_AREAS.MAX_PRICE]: 'Preço teto',
+  [HEAD_AREAS.RECOMMENDATION]: 'Recomendação',
+  [HEAD_AREAS.ALOCATION]: 'Alocação',
+};
