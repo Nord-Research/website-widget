33f5755f6a8900555c154fd6029f57f725e9ddb2
Maicon Silva <maiconrs95@gmail.com>
2021-07-18 00:21:26 -0300

    feat(recommendations): added hook to get recommendations

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useEquities } from './use-equities';
 export { default as useIndicators } from './use-indicators';
+export { default as useGetRecommendations } from './use-get-recommendations';
---
+++ src/hooks/use-get-recommendations/index.js
@@ -0,0 +1,3 @@
+export const useGetRecommendations = () => {};
+
+export default useGetRecommendations;
