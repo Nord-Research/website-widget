00ae8427803ea674cb5eb3f1e651d4763ebed754
Maicon Silva <maiconrs95@gmail.com>
2021-07-19 18:54:15 -0300

    feat(recommendations): added exclusive wallet view

---
+++ src/recommendations/components/ExclusiveWallet/index.js
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+
+import * as S from './styles';
+
+export const ExclusiveWallet = () => (
+  <S.Container>
+    <S.Title>Carteira exclusiva para assinantes do pacote</S.Title>
+    <S.Button>Assine agora</S.Button>
+  </S.Container>
+);
+
+export default ExclusiveWallet;
